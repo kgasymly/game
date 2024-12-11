693b6fb5f56faf75ec1c3bc1076c9a5155420401
Kanan <you@example.com>
2024-12-11 12:45:52 +0300

    kanan

---
+++ src/main/java/ru/itis/game/Ant.java
@@ -0,0 +1,11 @@
+package ru.itis.game;
+
+public class Ant extends Entity {
+    public Ant() {}
+    public Ant(int x, int y) {
+        super("Муравей", x, y);
+    }
+
+    @Override
+    public void move() {}
+}
--- src/main/java/ru/itis/game/Entity.java
+++ src/main/java/ru/itis/game/Entity.java
@@ -1,4 +1,56 @@
 package ru.itis.game;
 
 public class Entity {
+    protected String name;
+    protected int x;
+    protected int y;
+    protected static int fieldWidth;
+    protected static int fieldHeigth;
+
+    public Entity() {}
+
+    public Entity(String name) {
+        this.name = name;
+    }
+
+    public Entity(String name, int x , int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    public void move() {}
+
+    public String toString() {
+        return  this.name + " "+ this.x + ", " + this.y + ")";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setFieldSize(int width, int heigth) {
+        fieldWidth = width;
+        fieldHeigth = heigth;
+    }
+
 }
